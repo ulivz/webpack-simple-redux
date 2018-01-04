8f8e362d218c5143ffa44699774b8e6a35a7a9bd
ulivz <472590061@qq.com>
2018-01-05 00:12:51 +0800

    feat: make it work

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,41 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
-export default class Content extends React.Component {
-
+class Counter extends Component {
 	constructor(props) {
-		super(props)
-		this.state = {
-			count: 0
+		super(props);
+		this.incrementAsync = this.incrementAsync.bind(this);
+		this.incrementIfOdd = this.incrementIfOdd.bind(this);
+	}
+
+	incrementIfOdd() {
+		if (this.props.value % 2 !== 0) {
+			this.props.onIncrement()
 		}
 	}
 
-	increment() {
-		console.log(this)
-		this.setState({
-			count: this.state.count + 1
-		})
-	}
-
-	decrement() {
-		this.setState({
-			count: this.state.count - 1
-		})
-	}
-
-	get evenOrOdd() {
-		return
+	incrementAsync() {
+		setTimeout(this.props.onIncrement, 1000)
 	}
 
 	render() {
+		const { value, onIncrement, onDecrement } = this.props
 		return (
-			<div id="counter">
-				Clicked: times, count is {this.state.count}
-				<button onClick={this.increment.bind(this)}>+</button>
-				<button onClick={this.decrement.bind(this)}>-</button>
-				<button>Increment if odd</button>
-				<button>Increment async</button>
-			</div>
-		);
+			<p>
+				Clicked: {value} times
+				{' '}
+				<button onClick={onIncrement}>
+					+
+				</button>
+				{' '}
+				<button onClick={onDecrement}>
+					-
+				</button>
+				{' '}
+				<button onClick={this.incrementIfOdd}>
+					Increment if odd
+				</button>
+				{' '}
+				<button onClick={this.incrementAsync}>
+					Increment async
+				</button>
+			</p>
+		)
 	}
+}
 
-};
+Counter.propTypes = {
+	value: PropTypes.number.isRequired,
+	onIncrement: PropTypes.func.isRequired,
+	onDecrement: PropTypes.func.isRequired
+}
+
+export default Counter
